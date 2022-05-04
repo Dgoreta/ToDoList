f1abbf173ee42d3fc4b52684da1c7e75b23e2eb0
Domagoj <dgoreta231@gmail.com>
2022-05-04 13:56:18 +0200

    amp test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import React,{ useState, useEffect } from 'react';
-import './App.css';
-import Form from './komponente/Form';
-import TodoList from './komponente/TodoList';
-import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import { API, graphqlOperation } from 'aws-amplify'
+/* src/App.js */
+import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from './aws-exports';
+import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 const initialState = { name: '', description: '' }
 
-function App({ signOut, user }) {
+const App = () => {
   const [formState, setFormState] = useState(initialState)
+  const [todos, setTodos] = useState([])
 
-  const [inputText,setInputText]=useState("");
-  const [todos,setTodos]=useState([]);
-  const [status,setStatus]=useState("all");
-  const [filteredTodos,setFilteredTodos]=useState([]);
+  useEffect(() => {
+    fetchTodos()
+  }, [])
 
-
-  useEffect(()=>{
-    fetchTodos();
-  },[])
-  useEffect(()=>{
-    filterHandler();
-    saveLocalTodos();
-  },[todos,status]);
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
+  }
 
   async function fetchTodos() {
     try {
@@ -51,61 +43,48 @@ function App({ signOut, user }) {
     }
   }
 
-  const filterHandler=()=>{
-    switch (status){
-      case 'completed':
-        setFilteredTodos(todos.filter(todo=> todo.completed===true));
-        break;
-        case 'uncompleted':
-          setFilteredTodos(todos.filter(todo=> todo.completed===false));
-          break;
-        default:
-          setFilteredTodos(todos);
-          break;
-    }
-  };
-  const saveLocalTodos=()=>{
-        if(todos.length!==0){
-          localStorage.setItem("todos", JSON.stringify(todos));
-        }
-}
-  const getLocalTodos=()=>{
-    if(localStorage.getItem("todos")===null){
-      localStorage.setItem("todos", JSON.stringify([]));
-
-    }else{
-     let todoLocal= JSON.parse(localStorage.getItem("todos"));
-     setTodos(todoLocal);
-
-    }
-  }
   return (
-
-  <div className="App">
-        <header>
-          <div>
-          <h1>{user.username} ToDo Lista V2</h1>
-          <button className="button" onClick={signOut}>Sign out</button>
-          </div>
-
-        </header>
-        <div className="todoList">
-        <Form 
-        todos={todos} 
-        setTodos={setTodos} 
-        inputText={inputText} 
-        setInputText={setInputText}
-        status={status}
-        setStatus={setStatus}/>
-        <TodoList todos={todos} 
-        setTodos={setTodos}
-        filteredTodos={filteredTodos}/>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div style={styles.container}>
+          <h1>Hello {user.username}</h1>
+          <button style={styles.button} onClick={signOut}>Sign out</button>
+          <br />
+          <h2>Amplify Todos</h2>
+          <input
+            onChange={event => setInput('name', event.target.value)}
+            style={styles.input}
+            value={formState.name}
+            placeholder="Name"
+          />
+          <input
+            onChange={event => setInput('description', event.target.value)}
+            style={styles.input}
+            value={formState.description}
+            placeholder="Description"
+          />
+          <button style={styles.button} onClick={addTodo}>Create Todo</button>
+          {
+            todos.map((todo, index) => (
+              <div key={todo.id ? todo.id : index} style={styles.todo}>
+                <p style={styles.todoName}>{todo.name}</p>
+                <p style={styles.todoDescription}>{todo.description}</p>
+              </div>
+            ))
+          }
         </div>
-
-
-    </div>
-
+      )}
+    </Authenticator>
   );
 }
 
-export default withAuthenticator(App);
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+}
+
+export default App
